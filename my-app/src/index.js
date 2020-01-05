ea5ddb6702f5d62a4e21b2770c9afbf45afdd0a4
d0riven <wega315@gmail.com>
2020-01-05 17:29:05 +0900

    change hard code to dynamic build square component at Board

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -17,6 +17,7 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
+        key={i}
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
       />
@@ -24,25 +25,15 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    let lines = [];
+    for (let row = 0; row < 3; row++) {
+      let line = [];
+      for (let col = 0; col < 3; col++) {
+        line.push(this.renderSquare(row * 3 + col))
+      }
+      lines.push(<div key={row} className="board-row">{line}</div>);
+    }
+    return <div>{lines}</div>;
   }
 }
 
